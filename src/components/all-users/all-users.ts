fc2c489d39e8e4cc0e3966b3ab7d697ced286e2b
Abderlahman-Omar <159623837+Abderlahman-Omar@users.noreply.github.com>
2025-06-29 12:27:28 +0300

    all users component

---
+++ src/components/all-users/all-users.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { UserApiService } from '../../services/user-api-service';
+import { response } from 'express';
+import { User } from '../../types/user';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-all-users',
+  imports: [CommonModule, RouterModule],
+  templateUrl: './all-users.html',
+  styleUrl: './all-users.css'
+})
+export class AllUsers implements OnInit {
+  allUsers:User[] = []
+  isLoading = true;
+  error: string | null = null;
+  constructor(private userApiService : UserApiService){}
+  ngOnInit(): void {
+   this.getAllUsersData()
+  }
+  private getAllUsersData():void{
+    this.userApiService.getAllUsers().subscribe({
+      next : (response) => {
+        this.allUsers = response
+        this.isLoading = false
+      },
+      error: (err) => {
+        this.error = "failed to fetch users data"
+        this.isLoading = false
+      }
+    })
+  }
+}
---
+++ src/components/single-user/single-user.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-single-user',
+  imports: [],
+  templateUrl: './single-user.html',
+  styleUrl: './single-user.css'
+})
+export class SingleUser {
+
+}
