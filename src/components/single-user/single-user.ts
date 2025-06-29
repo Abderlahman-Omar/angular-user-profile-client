b705a21b5567514aa852486593427cb0cd7c5249
Abderlahman-Omar <159623837+Abderlahman-Omar@users.noreply.github.com>
2025-06-29 12:28:33 +0300

    single user component

--- src/components/single-user/single-user.ts
+++ src/components/single-user/single-user.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, RouterModule } from '@angular/router';
+import { UserApiService } from '../../services/user-api-service';
+import { User } from '../../types/user';
 
 @Component({
   selector: 'app-single-user',
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './single-user.html',
   styleUrl: './single-user.css'
 })
-export class SingleUser {
-
+export class SingleUser implements OnInit {
+  userId!:number
+  userDetails?:User
+  isLoading = true;
+  error: string | null = null;
+  constructor(
+    private userApiService:UserApiService,
+    private activatedRoute:ActivatedRoute
+  ){}
+  ngOnInit(): void {
+    this.activatedRoute.paramMap.subscribe((params) => {
+      this.userId = Number(params.get('id'))
+    })
+    this.getSingleUserData(this.userId)
+  }
+  private getSingleUserData(id:number):void{
+    this.userApiService.getUserById(id).subscribe({
+      next : (response) => {
+        this.userDetails = response
+        this.isLoading = false
+      },
+      error: (err) => {
+        this.error = "failed to fetch users data"
+        this.isLoading = false
+      }
+    })
+  }
 }
